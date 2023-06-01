c24de64d83f75b0e4c96c2cccfd632062c960b9f
tuche24 <tuche24@naver.com>
2023-06-01 11:33:30 +0900

    Initial Commit

---
+++ MovingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovingView
+//
+//  Created by 이도영 on 2023/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
